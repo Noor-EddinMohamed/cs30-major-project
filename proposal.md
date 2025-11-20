0027b4fd1ebde728eab626f1f54aaea38852ae2a
Noor-EddinMohamed <nooreddinmohamed@gmail.com>
2025-11-20 09:35:25 -0600

    updated propsal.md and began making 2d array for project

--- proposal.md
+++ proposal.md
@@ -6,8 +6,12 @@
 ## Needs to Have
 - at least 3 levels of progressing difficulty 
 - at least 5 unique contraptions
+- realistic physics (using p5play)
+- start screen / level selector
+- sound effects
+
 
 ## Nice to Have
 - level creator
-- start screen / level selector
-- sound/visual effects
+- visual effects
+- nice graphics
--- sketch.js
+++ sketch.js
@@ -1,14 +1,42 @@
 // Rube Goldberg Machine Game
 // Noor-Eddin Mohamed
 // Date
-// Resources:
-// https://www.youtube.com/watch?v=IQmFqhZ11K8
+
+let theGrid;
+const SQUARE_DIMENSIONS = 4;
+let cellSize = 100;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  theGrid = generateEmptyGrid(SQUARE_DIMENSIONS, SQUARE_DIMENSIONS);
 }
 
 function draw() {
   background(220);
-  circle(mouseX, mouseY, 50);
+  showGrid();
+}
+
+function showGrid() {
+  for (let y = 0; y < SQUARE_DIMENSIONS; y++) {
+    for (let x = 0; x < SQUARE_DIMENSIONS; x++) {
+      if (theGrid[y][x] === 1) {
+        fill("black");
+      }
+      else if (theGrid[y][x] === 0) {
+        fill("white");
+      }
+      square(x * cellSize, y * cellSize, cellSize);
+    }
+  }
+}
+
+function generateEmptyGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++) {
+      newGrid[y].push(0);
+    }
+  }
+  return newGrid;
 }
