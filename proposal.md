317d4fd27c81c4e13bb27990587b9f3562917d1a
Noor-EddinMohamed <nooreddinmohamed@gmail.com>
2025-11-24 09:45:32 -0600

    added 2d grid again to sketch.js, created new folder for matter.js demos

--- proposal.md
+++ proposal.md
@@ -5,15 +5,14 @@
 
 ## Needs to Have
 - at least 3 levels of progressing difficulty 
-- at least 5 unique contraptions
+- at least 3 unique contraptions
 - placeable inventory of contraptions
 - realistic physics (using matterjs)
 - start screen / level selector
 - sound effects
-- visual effects
-- nice graphics
-
 
 ## Nice to Have
 - level editor (can download and run user-created levels)
 - scrolling screen
+- visual effects
+- nice graphics
--- sketch.js
+++ sketch.js
@@ -4,21 +4,56 @@
 
 // for matter.js
 let Engine = Matter.Engine,
-  Bodies = Matter.Bodies,
   Runner = Matter.Runner,
-  World = Matter.World,
+  Bodies = Matter.Bodies,
   Composite = Matter.Composite;
+
 let engine;
+
+let theGrid;
+const CELL_SIZE = 100;
+let rows;
+let cols;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
   // matter.js setup
-  engine = Matter.Engine.create(); // creates engine 
-  world = engine.world; // creates world for engine to run in
+  engine = Engine.create(); // creates engine 
   Runner.run(Runner, engine); // runs engine
+
+  cols = Math.floor(width / CELL_SIZE);
+  rows = Math.floor(height / CELL_SIZE);
+
+  theGrid = generateEmptyGrid(cols, rows);
 }
 
 function draw() {
   background(220);
+  showGrid();
+}
+
+function showGrid() {
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (theGrid[y][x] === 1) {
+        fill("black");
+      }
+      else if (theGrid[y][x] === 0) {
+        fill("white");
+      }
+      square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
+    }
+  }
+}
+
+function generateEmptyGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++) {
+      newGrid[y].push(0);
+    }
+  }
+  return newGrid;
 }
