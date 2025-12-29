2d9f5a9145a584397905fa80076ebb389f5cfdd3
Noor-EddinMohamed <nooreddinmohamed@gmail.com>
2025-12-29 16:19:47 -0600

    deleting objects, trying to debug trampoline restitution

--- sketch.js
+++ sketch.js
@@ -18,6 +18,7 @@ let cols;
 
 let wallArray = [];
 let ballArray = [];
+let contrArray = [];
 
 let setting = "wall";
 
@@ -48,6 +49,9 @@ function draw() {
   for (let someBall of ballArray) {
     someBall.display();
   }
+  for (let someContr of contrArray) {
+    someContr.display();
+  }
 }
 
 function mousePressed() {
@@ -64,9 +68,68 @@ function keyPressed() {
   else if (key === "b") {
     setting = "ball";
   }
+  else if (key === "t") {
+    setting = "tramp";
+  }
+  else if (keyCode === LEFT_ARROW) {
+    if (contrArray.length > 0) {
+      contrArray[contrArray.length - 1].rotateLeft();
+    }
+  }
+  else if (keyCode === RIGHT_ARROW) {
+    if (contrArray.length > 0) {
+      contrArray[contrArray.length - 1].rotateRight();
+    }
+  }
+
+}
+
+function getOccupiedCells(body) {
+  let bounds = body.bounds;
+  let occupied = [];
+
+  let startCol = Math.floor(bounds.min.x / CELL_SIZE);
+  let endCol   = Math.floor(bounds.max.x / CELL_SIZE);
+  let startRow = Math.floor(bounds.min.y / CELL_SIZE);
+  let endRow   = Math.floor(bounds.max.y / CELL_SIZE);
+
+  for (let i = startCol; i <= endCol; i++) {
+    for (let j = startRow; j <= endRow; j++) {
+      occupied.push({x: i * CELL_SIZE, y: j * CELL_SIZE});
+    }
+  }
+
+  return occupied;
+}
+
+function isCellOccupied(x, y) {
+  for (let w of wallArray) {
+    if (w.x === x && w.y === y) {
+      return true;
+    }
+  }
+  for (let b of ballArray) {
+    let occupiedCells = getOccupiedCells(b.body);
+    for (let cell of occupiedCells) {
+      if (cell.x === x && cell.y === y) {
+        return true;
+      }
+    }
+  }
+  for (let c of contrArray) {
+    if (c.x === x && c.y === y) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function toggleCell(x, y) {
+  if (isCellOccupied(x, y)) {
+    deleteCell(x, y);
+    return;
+  }
+  
   if (setting === "wall") {
     let theWall = new Wall(x, y);
     wallArray.push(theWall);
@@ -75,6 +138,37 @@ function toggleCell(x, y) {
     let theBall = new Ball(x, y);
     ballArray.push(theBall);
   }
+  else if (setting === "tramp") {
+    let theContr = new Trampoline(x, y, 0);
+    contrArray.push(theContr);
+  }
+}
+
+function deleteCell(x, y) {
+    for (let i = contrArray.length - 1; i >= 0; i--) {
+    if (contrArray[i].x === x && contrArray[i].y === y) {
+      Composite.remove(engine.world, contrArray[i].body);
+      contrArray.splice(i, 1);
+      return; 
+    }
+  }
+
+  for (let i = wallArray.length - 1; i >= 0; i--) {
+    if (wallArray[i].x === x && wallArray[i].y === y) {
+      Composite.remove(engine.world, wallArray[i].body);
+      wallArray.splice(i, 1);
+      return;
+    }
+  }
+
+  for (let i = ballArray.length - 1; i >= 0; i--) {
+    if (ballArray[i].x === x && ballArray[i].y === y) {
+      Composite.remove(engine.world, ballArray[i].body);
+      ballArray.splice(i, 1);
+      return;
+    }
+  }
+
 }
 
 function showGrid() {
@@ -104,7 +198,7 @@ class Ball {
     this.y = y;
     this.radius = CELL_SIZE / 2;
     this.color = "red";
-    let options = { restitution: 0.9, friction: 0.25 };
+    let options = { restitution: 0.5, friction: 0.1 };
     this.body = Bodies.circle(this.x, this.y, this.radius, options);
 
     Composite.add(engine.world, this.body);
@@ -139,6 +233,57 @@ class Wall {
     rectMode(CENTER);
     fill(this.color);
     square(this.x, this.y, this.width);
+    pop();
+  }
+}
+
+class Contraption {
+  constructor(x, y, angle) {
+    this.x = x;
+    this.y = y;
+    this.angle = angle;
+  }
+
+  rotateLeft() {
+    this.angle = (this.angle - 15) % 360;
+    Matter.Body.setAngle(this.body, this.angle * (Math.PI/180));
+
+  }
+  rotateRight() {
+    this.angle = (this.angle + 15) % 360;
+    Matter.Body.setAngle(this.body, this.angle * (Math.PI/180));
+
+  }
+}
+
+class Trampoline extends Contraption {
+  constructor(x, y, angle) {
+    super(x, y, angle)
+    this.color = "purple";
+    this.width = CELL_SIZE;
+    this.height = CELL_SIZE / 5;
+    let options = { restitution: 1, friction: 0.1, isStatic: true };
+    this.body = Bodies.rectangle(this.x, this.y, this.width, this.height, options);
+    Matter.Body.setAngle(this.body, this.angle * (Math.PI/180));
+
+    Composite.add(engine.world, this.body);
+
+  }
+
+  rotate() {
+    super.rotate();
+    Matter.Body.setAngle(this.body, this.angle * (Math.PI / 180));
+  }
+
+  display() {
+    let position = this.body.position;
+    let angle = this.body.angle;
+    push();
+    translate(position.x, position.y);
+    rotate(angle);
+    rectMode(CENTER);
+    fill(this.color);
+    rect(0, 0, this.width, this.height);
     pop();
   }
 }
