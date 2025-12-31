19ff723110740af3918ad79feaf538be973139d7
Noor-EddinMohamed <nooreddinmohamed@gmail.com>
2025-12-30 23:15:49 -0600

    debugged trampoline bouncing physics

--- sketch.js
+++ sketch.js
@@ -37,12 +37,59 @@ function setup() {
   rows = Math.floor(height / CELL_SIZE);
 
   theGrid = generateEmptyGrid(cols, rows);
+
+  // ball-trampoline collision function
+  function onCollision(event) {
+    for (let pair of event.pairs) {
+      handleTrampCollision(pair);
+    }
+  }
+  Matter.Events.on(engine, "collisionStart", onCollision);
+}
+
+function handleTrampCollision(pair) {
+  let bodyA = pair.bodyA;
+  let bodyB = pair.bodyB;
+
+  let ball = null;
+  let tramp = null;
+
+  for (let b of ballArray) {
+    if (b.body === bodyA || b.body === bodyB) {
+      ball = b;
+      break;
+    }
+  }
+
+  for (let c of contrArray) {
+    if (c instanceof Trampoline) {
+      if (c.body === bodyA || c.body === bodyB) {
+        tramp = c;
+        break;
+      }
+    }
+  }
+
+  if (!ball || !tramp) return;
+
+  let angle = tramp.body.angle;
+
+  // bounce physics
+  let vyBall = ball.body.velocity.y;
+  let speed = Math.max(10, Math.abs(vyBall) * 1.2);; 
+
+  let vx = Math.sin(angle) * speed;
+  let vy = -Math.cos(angle) * speed;
+
+  Matter.Body.setVelocity(ball.body, { x: vx, y: vy });
 }
 
 function draw() {
   background("white");
   Engine.update(engine);
+  deleteOutOfBounds();
   showGrid();
+
   for (let someWall of wallArray) {
     someWall.display();
   }
@@ -57,6 +104,10 @@ function draw() {
 function mousePressed() {
   let x = Math.round(mouseX / CELL_SIZE) * CELL_SIZE;
   let y = Math.round(mouseY / CELL_SIZE) * CELL_SIZE;
+
+  if (!isInsideGrid(x, y)) {
+    return;
+  }
 
   toggleCell(x, y);
 }
@@ -81,10 +132,19 @@ function keyPressed() {
       contrArray[contrArray.length - 1].rotateRight();
     }
   }
+}
 
+function isInsideGrid(x, y) {
+  return (
+    x >= 0 &&
+    y >= 0 &&
+    x < cols * CELL_SIZE &&
+    y < rows * CELL_SIZE
+  );
 }
 
 function getOccupiedCells(body) {
+  // can't place stuff outside cells
   let bounds = body.bounds;
   let occupied = [];
 
@@ -108,14 +168,6 @@ function isCellOccupied(x, y) {
       return true;
     }
   }
-  for (let b of ballArray) {
-    let occupiedCells = getOccupiedCells(b.body);
-    for (let cell of occupiedCells) {
-      if (cell.x === x && cell.y === y) {
-        return true;
-      }
-    }
-  }
   for (let c of contrArray) {
     if (c.x === x && c.y === y) {
       return true;
@@ -125,7 +177,7 @@ function isCellOccupied(x, y) {
 }
 
 function toggleCell(x, y) {
-  if (isCellOccupied(x, y)) {
+  if (isCellOccupied(x, y)) { // delete if something already on that cell
     deleteCell(x, y);
     return;
   }
@@ -168,7 +220,22 @@ function deleteCell(x, y) {
       return;
     }
   }
+}
 
+function deleteOutOfBounds() {
+  for (let i = ballArray.length - 1; i >= 0; i--) { // delete if ball goes out of bounds
+    let b = ballArray[i];
+    let pos = b.body.position;
+
+    if (
+      pos.y > rows * CELL_SIZE + 200 || 
+      pos.x < -200 ||
+      pos.x > cols * CELL_SIZE + 200
+    ) {
+      Composite.remove(engine.world, b.body);
+      ballArray.splice(i, 1);
+    }
+  }
 }
 
 function showGrid() {
@@ -200,6 +267,8 @@ class Ball {
     this.color = "red";
     let options = { restitution: 0.5, friction: 0.1 };
     this.body = Bodies.circle(this.x, this.y, this.radius, options);
+    this.body.label = "ball";
+
 
     Composite.add(engine.world, this.body);
   }
@@ -252,7 +321,6 @@ class Contraption {
   rotateRight() {
     this.angle = (this.angle + 15) % 360;
     Matter.Body.setAngle(this.body, this.angle * (Math.PI/180));
-
   }
 }
 
@@ -262,8 +330,9 @@ class Trampoline extends Contraption {
     this.color = "purple";
     this.width = CELL_SIZE;
     this.height = CELL_SIZE / 5;
-    let options = { restitution: 1, friction: 0.1, isStatic: true };
+    let options = { isStatic: true };
     this.body = Bodies.rectangle(this.x, this.y, this.width, this.height, options);
+    this.body.label = "trampoline";
     Matter.Body.setAngle(this.body, this.angle * (Math.PI/180));
 
     Composite.add(engine.world, this.body);
